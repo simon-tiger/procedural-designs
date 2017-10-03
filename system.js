c96cbb9f84ae94be70cff3926919b59319200986
Simon Tiger <monajune@gmail.com>
2017-10-03 15:21:53 +0200

    Adding some useful comments

--- system.js
+++ system.js
@@ -1,8 +1,12 @@
+// Publisher
 function publish(sketch, designName, authorName) {
+  // Create the <div> element
+  // for the design
   let div = document.createElement("div");
   document.getElementById("designs").appendChild(div);
   let design = new p5(sketch, div);
-
+  
+  // Adding some info
   design.createElement("h2", designName);
   design.createElement("h4", "by " + authorName);
 }
