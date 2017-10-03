0d0ef57f7609476ec8cd6ff58350d58de25da0dc
simon-tiger <monajune@gmail.com>
2017-10-03 15:13:37 +0200

    Adding some styling (My second try...)

--- system.js
+++ system.js
@@ -1,6 +1,5 @@
 function publish(sketch, designName, authorName) {
   let div = document.createElement("div");
-  div.className = "w3-panel w3-white";
   document.getElementById("designs").appendChild(div);
   let design = new p5(sketch, div);
 
