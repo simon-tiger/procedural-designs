2949f8ab3bd30f2ab347bee9dddcf2f8b214b5f6
Simon Tiger <monajune@gmail.com>
2017-10-05 22:44:17 +0200

    Adding a wiggling cloud example

--- designs.js
+++ designs.js
@@ -21,5 +21,62 @@ window.onload = () => {
   //  designName (the title of the design)
   //  authorName (the name of the author of the design)
   publish(rectangle, "Example", "Example");
+  
+  /* -------- Add your own designs below! -------- */
+  
+  function wigglingCloud(p) {
+    p.yoff = 0;
+    
+    p.setup = function() {
+      p.createCanvas(600, 600);
+    }
+    
+    p.draw = function() {
+      p.background(175, 205, 255);
+      p.fill(255);
+      p.noStroke();
+      
+      // The real bit
+  
+      // How many points?
+      let num = 256;
+      let xoff = 0;
+      
+      let r = 200;
+      let rZero = r + 50 * p.noise(0, p.yoff);
+      
+      p.beginShape(); // Draw the shape
+      for (let i = 0; i < num; i++) {
+        
+        let a = p.map(i, 0, num, 0, p.TWO_PI);
+        
+        // Perlin Noise
+        let n = r + 50 * p.noise(xoff, p.yoff);
+        
+        // Making it a loop (so you don't
+        // see any cut on the right)
+        if (i == 0) {
+          rZero = n;
+        } else if (i > num*0.95) {
+          let percent = p.map(i, num*0.95, num, 0, 1);
+          n = p.lerp(n, rZero, percent);
+        }
+        
+        // Polar to Cartesian
+        // coordinate transformation
+        let x = p.width/2 + n * p.cos(a);
+        let y = p.height/2 + n * p.sin(a);
+        
+        vertex(x, y);
+    
+        xoff += 0.1;
+      }
+      endShape(CLOSE);
+      
+      yoff += 0.05;
+    }
+  }
+  
+  publish(wigglingCloud, "Wiggling Cloud", "Simon Tiger Houben");
 
 }
